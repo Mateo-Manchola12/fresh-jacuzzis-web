d4f2cc3a44dead08ff41b5bf5f4b8b9e7da17fd7
Mateo Manchola <mmanchola12temporal@gmail.com>
2025-04-06 18:31:26 +0200

    Implementacion linter y prettier

--- src/constants/static_paths.ts
+++ src/constants/static_paths.ts
@@ -2,9 +2,7 @@ type StaticPath = string;
 
 export const DOMAIN_NAME: StaticPath = import.meta.env.PROD
     ? "https://www.freshjacuzzis.com"
-    : "http://localhost:4321";
-
-console.log(DOMAIN_NAME);
+    : "http://192.168.1.141:4321";
 
 export const CTA_TARGET: StaticPath =
     "https://wa.me/573126799235?text=Hola%2C%20he%20descubierto%20su%20sitio%20web%20y%20me%20ha%20fascinado%20su%20oferta.%20Me%20gustaría%20cotizar%20un%20jacuzzi";
