ca77ba1f5a7b75f2a1c9c69e54edbe8e637f8b53
Mateo Manchola <mmanchola12temporal@gmail.com>
2025-07-10 18:36:55 +0200

    Release 2.0

---
+++ src/types/pageTypes.d.ts
@@ -0,0 +1,18 @@
+type SinglePage = {
+    name: string
+    path?: string
+    slug: string
+    clickable?: true
+    description: string
+}
+
+type ParentPage = {
+    name: string
+    path?: string
+    slug: string
+    description: string
+    clickable?: false
+    subpages: Page[]
+}
+
+export type Page = SinglePage | ParentPage
